991953a8ed40cf51039eb913eebfc1d969feef35
Stefanie St. Germain (desktop) <stefanie_stgermain@harvard.edu>
2014-10-24 08:25:40 -0400

    add desk image toggle to open or closed

--- js/application.js
+++ js/application.js
@@ -12,4 +12,38 @@
 	backgroundColor: "green"
        }); //end p css
        $('footer ul li:last, nav ul li:first, aside ul li:last').fadeOut(500).fadeIn(1000);
+  
+   $('#btn-furniture').click(function(){
+      console.log('The furniture button was clicked!');
+      alert('Nice furniture!');
+   });  //end furniture click
+ 
+   //hover 2 parts: what happens when mouse is over and what happens when goes out  
+   $('#btn-papercraft').hover(function(){
+      console.log('The papercraft button was hovered over');
+      $('#btn-papercraft').css({
+         backgroundColor: 'green'
+      }); //end css
+   }, function(){
+      console.log('The papercraft button was left behind');
+      $('#btn-papercraft').css({
+         backgroundColor: 'blue'
+      }); //end css
+   });  //end papercraft button
+   $('#desk-closed-img').hover(function(){
+   //on hover
+      $('#desk-closed-img').attr({
+         'src': 'images/desk_closed_150w.png',
+         'class': 'img-responsive img-circle' //does not add to existing so reset everything associate with classes on the image
+      }); //end attr
+   }, // end mouse over
+  function(){
+   //off hover
+      $('#desk-closed-img').attr({
+         'src': 'images/desk_open_150w.png',
+         'class': 'img-responsive' //does not add to existing so reset everything associate with classes on the image
+      }); //end attr
+   
+   }); //end desk image hover
+
    }); //end ready
